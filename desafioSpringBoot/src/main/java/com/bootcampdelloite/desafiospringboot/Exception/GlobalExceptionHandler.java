70d04562b5f13f6afd42c52f2adfdb4d04e32c21
Brenno Felipe <devbrenno1907@gmail.com>
2026-03-10 13:03:35 -0300

    feat: adicionar GlobalExceptionHandler para tratar erros da api

---
+++ desafioSpringBoot/src/main/java/com/bootcampdelloite/desafiospringboot/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.bootcampdelloite.desafiospringboot.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    @ExceptionHandler(UsuarioException.class)
+    public ResponseEntity<String> handleUsuarioException(UsuarioException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Erro: " + e.getMessage());
+    }
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGenericException(Exception e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Erro inesperado: " + e.getMessage());
+    }
+}
+
