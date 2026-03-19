ad6dc22a1763107bc5227dbaa3f96e539eca6faa
Brenno Felipe <devbrenno1907@gmail.com>
2026-03-19 17:35:43 -0300

    refactor: adicionar método validar(dto, idIgnorar) em UsuarioValidation.java, EmailDuplicadoValidation.java e NomeDuplicadoValidation.java

--- desafioSpringBoot/src/main/java/com/bootcampdelloite/desafiospringboot/Validation/UsuarioValidation.java
+++ desafioSpringBoot/src/main/java/com/bootcampdelloite/desafiospringboot/Validation/UsuarioValidation.java
@@ -4,4 +4,8 @@ import com.bootcampdelloite.desafiospringboot.Dto.UsuarioRequestDTO;
 
 public interface UsuarioValidation {
     void validar(UsuarioRequestDTO dto);
+
+    default void validar(UsuarioRequestDTO dto, Long idIgnorar) {
+        validar(dto);
+    }
 }
